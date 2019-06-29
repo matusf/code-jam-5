8719be8c3743701b993595f7945efa5d0793d6a8
Blue Phoenix <gabriel.nordholm@gmail.com>
2019-06-29 22:22:48 +0200

    Define new start_window function Which will run all code needed to start the window. To easily be able to comment it out when testing.

--- young_yacks/start.py
+++ young_yacks/start.py
@@ -2,9 +2,14 @@ import arcade
 
 from window import GameWindow
 
-if __name__ == "__main__":      # Should not be imported
+
+def start_window():
     game = GameWindow(1680, 1050, "Our Game!")
     game.setup()
 
     # Will keep the program running, until the user closes the window
     arcade.run()
+
+
+if __name__ == "__main__":      # Should not be imported
+    start_window()
