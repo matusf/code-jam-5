e39b777a38faba4527af8ca65dd52f9cf0f571fe
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-06-29 11:47:48 +0200

    changed floats to ints and some attribute renaming

--- young_yacks/game/models.py
+++ young_yacks/game/models.py
@@ -12,23 +12,23 @@ class EarthSystem:
 class CowBreed:
     name: str
     upgrade_level: int
-    income_rate: int
-    water_pollution_rate: int
-    greenhouse_gas_rate: int
-    cost: int
+    value: float
+    water_pollution_rate: float
+    greenhouse_gas_rate: float
+    cost: float
     total_cows: int
 
 
 class Farm:
-    income: int
+    income: float
     cow_types: List[CowBreed]
     systems: List[EarthSystem]
 
     @staticmethod
-    def calculate_income(cow_breeds: List[CowBreed]) -> int: pass
+    def get_income_rate(cow_breeds: List[CowBreed]) -> float: pass
 
     @staticmethod
-    def calculate_environment_damage(cow_breeds: List[CowBreed], systems: List[EarthSystem]) -> List[EarthSystem]: pass
+    def get_environment_damage_rate(cow_breeds: List[CowBreed], systems: List[EarthSystem]) -> List[EarthSystem]: pass
 
 
 class Game:
