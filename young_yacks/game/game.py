7d31d0c2a5eeb59d74aaf48a1f656eefc26aefce
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-07-01 07:22:27 +0200

    style changed to one-line declaration

--- young_yacks/game/game.py
+++ young_yacks/game/game.py
@@ -1,39 +1,12 @@
 from .models import EarthMetric, CowBreed, Game, Player
 
 
-
 water_level = EarthMetric(name='Water Level', unit='m', min=20, max=22, level=20.5)
 temperature = EarthMetric(name='Air Temperature', unit='°C', min=10, max=40, level=15)
 
-meat_cows = CowBreed(
-    name="Meat Cows",
-    damage_rates={
-        water_level: 10,
-        temperature: 15
-    },
-    value=20,
-    cost=100
-)
-
-milk_cows = CowBreed(
-    name="Milk Cows",
-    damage_rates={
-        water_level: 20,
-        temperature: 5
-    },
-    value=30,
-    cost=150
-)
-
-burp_cows = CowBreed(
-    name="Milk Cows",
-    damage_rates={
-        water_level: 5,
-        temperature: 50
-    },
-    value=40,
-    cost=30
-)
+meat_cows = CowBreed(name="Meat Cows", damage_rates={water_level: 10, temperature: 15}, value=20, cost=100)
+milk_cows = CowBreed(name="Milk Cows", damage_rates={water_level: 20, temperature: 5}, value=30, cost=150)
+burp_cows = CowBreed(name="Milk Cows", damage_rates={water_level: 5, temperature: 50}, value=40, cost=30)
 
 
 def build_game():
