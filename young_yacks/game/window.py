0102ffeb1c98b42c82f2c630e5e23a8cd524e84e
Blue Phoenix <gabriel.nordholm@gmail.com>
2019-07-01 23:37:07 +0200

    Create custom TextButton class and replace button

--- young_yacks/game/window.py
+++ young_yacks/game/window.py
@@ -1,9 +1,5 @@
 import arcade
-
-
-def _has_been_clicked(x, y, sprite):
-    """ Returns a True if the coordinates are inside the sprite."""
-    return sprite.left < x < sprite.right and sprite.bottom < y < sprite.top
+from .textbutton import TextButton
 
 
 class GameWindow(arcade.Window):
@@ -16,8 +12,8 @@ class GameWindow(arcade.Window):
         super().__init__(width, height, title, fullscreen, resizable)
 
         arcade.set_background_color(arcade.color.AMAZON)
-        
-        self.start_buttons = None
+
+        self.textbutton = None
 
     def setup(self):
         """ Set up the game and initialize the variables.
@@ -26,12 +22,9 @@ class GameWindow(arcade.Window):
 
     def start_menu(self):
         """ Will set up the window like the start menu """
-        self.start_buttons = arcade.SpriteList()
-
-        # I named the image cow.png but it is just a black box.
-        self.start_button = arcade.Sprite("cow.png", 1)
-        self.start_button.center_x, self.start_button.center_y = self.width/2, self.height/2
-        self.start_buttons.append(self.start_button)
+        self.textbutton = TextButton(x=self.width/2, y=self.height/2, width=300, height=300,
+                                     text="Clicks ;)", font_size=12.0, color=(150, 150, 150),
+                                     text_color=(0, 0, 0), filled=True)
 
     # Events
 
@@ -40,9 +33,7 @@ class GameWindow(arcade.Window):
         Render the screen.
         """
         arcade.start_render()
-
-        self.start_buttons.draw()
-        arcade.draw_text("Click Me!", self.start_buttons[0].left, self.start_buttons[0].bottom, arcade.color.WHITE, 31)
+        self.textbutton.draw()
 
     def update(self, delta_time):
         """
@@ -77,7 +68,7 @@ class GameWindow(arcade.Window):
         """
         Called when the user presses a mouse button.
         """
-        print(_has_been_clicked(x, y, self.start_buttons[0]))
+        print(self.textbutton.is_inside(x, y))
 
     def on_mouse_release(self, x, y, button, key_modifiers):
         """
