268e0cab37af3e7571e2a37cc470f390db762832
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-07-01 07:36:29 +0200

    removed rules.py, because it wasn't really helping much.

--- young_yacks/game/__init__.py
+++ young_yacks/game/__init__.py
@@ -1,4 +1,3 @@
 from .models import CowBreed, EarthMetric, Player, Game
-from . import rules
 from . import game
 from .game import build_game
--- young_yacks/game/models.py
+++ young_yacks/game/models.py
@@ -11,8 +11,9 @@ class EarthMetric:
     max: float = field(repr=False)
 
     @property
-    def damage_perc(self) -> float:
-        return rules.get_system_damage_perc(system=self)
+    def level_perc(self) -> float:
+        """Returns the level's percentage between the min and max values"""
+        return (self.level - self.min) / (self.max - self.min)
 
 
 @dataclass(unsafe_hash=True)
@@ -27,48 +28,43 @@ class CowBreed:
 @dataclass
 class Player:
     systems: List[EarthMetric] = field(repr=False)
-    win_threshold: float = field(repr=False)
-    lose_threshold: float = field(repr=False)  # must be higher than win_threshold
     money: float = field(default=0)
     cows: List[CowBreed] = field(default_factory=list)
 
     @property
     def income(self):
         """The player's income rate."""
-        return rules.get_income(cow_breeds=self.cows)
+        return sum(breed.value * breed.total_cows for breed in self.cows)
 
     @property
     def damages(self) -> Dict[EarthMetric, float]:
-        """The rate of damage on each systems, when all cows are taken into account."""
-        return rules.get_environment_damage(cow_breeds=self.cows, systems=self.systems)
+        """The rate of damage on each system, when all cows are taken into account."""
+        damage = {}
+        for system in self.systems:
+            damage[system] = sum(breed.damage_rates[system] for breed in self.cows)
+        return damage
 
     @property
     def earth_damage_perc(self) -> float:
-        """The earth's overall damage level, between 0 (unharmed) and 1 (harmed)."""
-        return rules.get_earth_damage_perc(systems=self.systems)
+        """The earth's overall damage level, between 0 (unharmed) and 1 (harmed), based on contributions from each system."""
+        damage = sum(system.level_perc for system in self.systems) / len(self.systems)
+        assert 0 <= damage <= 1
+        return damage
 
 
 @dataclass
 class Game:
     player: Player
     game_time: float
-    cow_breeds: List[CowBreed]
+    cow_breeds: List[CowBreed] = field(repr=False)
+    win_threshold: float
+    lose_threshold: float  # must be higher than win_threshold
 
     @property
     def was_won(self) -> bool:
-        return rules.player_scared_aliens(
-            doom_perc=self.player.earth_damage_perc,
-            win_threshold=self.player.win_threshold,
-            lose_threshold=self.player.lose_threshold,
-            game_time=self.game_time
-        )
+        return self.game_time <= 0 and self.win_threshold < self.player.earth_damage_perc < self.lose_threshold
 
     @property
     def was_lost(self):
-        lost1 = rules.player_destroyed_earth(
-            doom_perc=self.player.earth_damage_perc,
-            lose_threshold=self.player.lose_threshold,
-        )
-        out_of_time = self.game_time < 0.
-        return lost1 or out_of_time
+        return self.player.earth_damage_perc > self.lose_threshold or (self.game_time < 0. and not self.was_won)
 
