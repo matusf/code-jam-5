b13ea14dd90c6e8e78285bc946b06a2705a2481e
Blue Phoenix <gabriel.nordholm@gmail.com>
2019-07-05 00:46:02 +0200

    Move DrawList class into own file

--- young_yaks/game/window.py
+++ young_yaks/game/window.py
@@ -1,15 +1,4 @@
 import arcade
-from .background import Background
-from collections import UserList
-
-
-class DrawList(UserList):
-    """ A custom list tweaked to work with objects
-    that implement the draw method."""
-    def draw(self):
-        """ Draw all the items."""
-        for item in self.data:
-            item.draw()
 
 
 class GameWindow(arcade.Window):
