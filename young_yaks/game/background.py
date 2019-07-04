49dcb1c51049483d607e0b59306962e58b370bf8
Blue Phoenix <gabriel.nordholm@gmail.com>
2019-07-05 01:00:11 +0200

    Fix sematic error

--- young_yaks/game/background.py
+++ young_yaks/game/background.py
@@ -5,11 +5,8 @@ class Background:
     """ A class representing a window background."""
     def __init__(self, texture=None, color=None):
         self.texture = texture
-        # Here's why this class isn't a dataclass
-        if texture is None:
-            self.color = color
-        else:
-            raise UserWarning("`texture` and `color` cannot be set at the same time")
+        # We don't allow both attributes at the same time
+        self.colour = color if texture is None else None
 
     def draw(self):
         if self.texture:
