d46afbfd3c4a0bd2f9c031a04719afc79c85a21f
Blue Phoenix <gabriel.nordholm@gmail.com>
2019-07-05 00:45:16 +0200

    Remove unused events and testing code To be able to start with empty canvas

--- young_yaks/game/window.py
+++ young_yaks/game/window.py
@@ -1,5 +1,4 @@
 import arcade
-from .textbutton import TextButton
 from .background import Background
 from collections import UserList
 
@@ -23,18 +22,11 @@ class GameWindow(arcade.Window):
         super().__init__(width, height, title, fullscreen, resizable)
 
         arcade.set_background_color(arcade.color.AMAZON)
-        self.background = None
-        self.draw_list = None
-        self.start_draw_list = DrawList([
-            TextButton(x=self.width/2, y=self.height/2, width=300, height=300,
-                       text="Clicks ;)", font_size=12.0, color=(150, 150, 150),
-                       text_color=(0, 0, 0), filled=True)])
 
     def setup(self):
         """ Set up the game and initialize the variables.
         NOTE: This is ran here so we could restart the game."""
-        self.background = Background(color=arcade.color.AMAZON)
-        self.draw_list = self.start_draw_list
+        pass
 
     # Events
 
@@ -43,8 +35,6 @@ class GameWindow(arcade.Window):
         Render the screen.
         """
         arcade.start_render()
-        self.background.draw()
-        self.draw_list.draw()
 
     def update(self, delta_time):
         """
@@ -63,26 +53,8 @@ class GameWindow(arcade.Window):
         """
         pass
 
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
     def on_mouse_press(self, x, y, button, key_modifiers):
         """
         Called when the user presses a mouse button.
-        """
-        print(self.draw_list[0].is_inside(x, y))
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
         """
         pass
