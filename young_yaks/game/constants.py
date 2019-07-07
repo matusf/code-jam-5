df7bd1cca19f0f3bde803c5b70b6b0cb0310015e
Blue Phoenix <gabriel.nordholm@gmail.com>
2019-07-07 15:16:31 +0200

    Add constants to constants.py

--- young_yaks/game/constants.py
+++ young_yaks/game/constants.py
@@ -1,9 +1,21 @@
 
 # Window Settings
-SCREEN_WIDTH: int = 640
-SCREEN_HEIGHT: int = 360
+SCREEN_WIDTH: int = 320
+SCREEN_HEIGHT: int = 180
 
 SCREEN_NAME: str = "Our Game!"
 SCREEN_FULLSCREEN: bool = False
 SCREEN_RESIZE: bool = False
 SCREEN_ALIASING: bool = False
+
+# Start Menu Settings
+START_MENU_COWS: int = 8
+
+# Meat Cow Settings
+MEAT_COW_IMAGE: str = "game/assets/cow.png"
+
+# Milk Cow Settings
+MILK_COW_IMAGE: str = "game/assets/cow.png"
+
+# Burp Cow Settings
+BURP_COW_IMAGE: str = "game/assets/cow.png"
