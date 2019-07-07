f849c514ca6ce581bad3bbe452e1c270ceab8936
Blue Phoenix <gabriel.nordholm@gmail.com>
2019-07-07 23:22:20 +0200

    Remove buttons

--- young_yaks/game/window.py
+++ young_yaks/game/window.py
@@ -39,63 +39,11 @@ class GameWindow(arcade.Window):
         # All variables are later defined in setup.
         # This is so we can restart the game.
         self.background: background.Background
-        self.button_list = None
 
     def setup(self):
         """ Set up the game and initialize the variables.
         NOTE: This is ran here so we could restart the game."""
         self.background = background.StartBackground()  # We always start in the start menu
-        self.game = Game.init_game()
-        self.button_list = [
-            ActionButton(
-                action=partialmethod(self.game.buy_cow, "Meat Cows"),
-                center_x=30,
-                center_y=10,
-                width=80,
-                height=30,
-                text="Buy Meat Cow"
-            ),
-            ActionButton(
-                action=partialmethod(self.game.buy_cow, "Milk Cows"),
-                center_x=30,
-                center_y=50,
-                width=80,
-                height=30,
-                text="Buy Milk Cow"
-            ),
-            ActionButton(
-                action=partialmethod(self.game.buy_cow, "Meat Burp"),
-                center_x=30,
-                center_y=70,
-                width=80,
-                height=30,
-                text="Buy Burp Cow"
-            ),
-            ActionButton(
-                action=partialmethod(self.game.sell_cow, "Meat Cows"),
-                center_x=100,
-                center_y=10,
-                width=80,
-                height=30,
-                text="Buy Meat Cow"
-            ),
-            ActionButton(
-                action=partialmethod(self.game.sell_cow, "Milk Cows"),
-                center_x=100,
-                center_y=50,
-                width=80,
-                height=30,
-                text="Buy Milk Cow"
-            ),
-            ActionButton(
-                action=partialmethod(self.game.sell_cow, "Meat Burp"),
-                center_x=100,
-                center_y=70,
-                width=80,
-                height=30,
-                text="Buy Burp Cow"
-            ),
-        ]
 
     # Events
 
@@ -104,9 +52,6 @@ class GameWindow(arcade.Window):
         Render the screen.
         """
         arcade.start_render()
-        self.background.draw()
-        for button in self.button_list:
-            button.draw()
 
     def update(self, delta_time):
         """
@@ -114,7 +59,7 @@ class GameWindow(arcade.Window):
         Normally, you'll call update() on the sprite lists that
         need it.
         """
-        self.game.update(delta_time)
+        pass
 
     def on_key_press(self, key, key_modifiers):
         """
@@ -124,20 +69,17 @@ class GameWindow(arcade.Window):
         http://arcade.academy/arcade.key.html
         """
         # If we're in the start "menu":
-        if isinstance(self.background, background.StartBackground):
-            self.background = background.GameBackground()
+        pass
 
     def on_mouse_press(self, x, y, button, key_modifiers):
         """
         Called when the user presses a mouse button.
         """
         # If we're in the start "menu":
-        if isinstance(self.background, background.StartBackground):
-            self.background = background.GameBackground()
-        check_mouse_press_for_buttons(x, y, self.button_list)
+        pass
 
     def on_mouse_release(self, x, y, button, key_modifiers):
         """
         Called when a user releases a mouse button.
         """
-        check_mouse_release_for_buttons(x, y, self.button_list)
+        pass
