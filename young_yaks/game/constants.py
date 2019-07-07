0e8a02d1fb99ebd32e5065dca557eb06f8f88a31
Blue Phoenix <gabriel.nordholm@gmail.com>
2019-07-07 14:22:21 +0200

    Remove model files We have now reset the project to it's base files

--- young_yaks/game/constants.py
+++ young_yaks/game/constants.py
@@ -7,15 +7,3 @@ SCREEN_NAME: str = "Our Game!"
 SCREEN_FULLSCREEN: bool = False
 SCREEN_RESIZE: bool = False
 SCREEN_ALIASING: bool = False
-
-# Background Settings
-BACKGROUND_TEXTURE_PATH: str = "folder/image.png"   # dummy value
-
-# Meat Cow Settings
-MEAT_COW_SPRITE_PATH: str = "folder/image.png"      # dummy value
-
-# Milk Cow Settings
-MILK_COW_SPRITE_PATH: str = "folder/image.png"      # dummy value
-
-# Burp Cow Settings
-BURP_COW_SPRITE_PATH: str = "folder/image.png"      # dummy value
